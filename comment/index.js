7e0c7a1f63e80afd6bf7a699950d7d0dc51c2551
weifng <2553039498@qq.com>
2022-10-17 19:56:03 +0800

    asdfasdfasdfasdfasdfasd

---
+++ comment/index.js
@@ -0,0 +1,5 @@
+import {name,age,sex} from "./student"
+console.log("name",name);
+console.log("age",age);
+console.log("sex",sex);
+// es5 严格模式 要复习 node运行要复习1
---
+++ comment/student.js
@@ -0,0 +1,8 @@
+// comment js 规范 规定了每个模块就是一个作用域 外部无法直接访问里面的变量 函数对象
+var name = "tom";
+var age = 19;
+var sex = "nan";
+export {name,age,sex}
+    
+    
+    
